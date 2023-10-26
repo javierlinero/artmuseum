a2b3e2fece90b8c0b81b093795c005d0e72852f5
Nathan Beck <natebeck18@gmail.com>
2023-10-25 23:27:33 -0400

    Implement center with setState first

--- mobile/lib/map/screens/map_screen.dart
+++ mobile/lib/map/screens/map_screen.dart
@@ -1,29 +1,55 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:puam_app/shared/index.dart';
 import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
+import 'package:geolocator/geolocator.dart';
 
-class MapPage extends StatelessWidget {
+class MapPage extends StatefulWidget {
+  @override
+  _MapPageState createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  LatLng? initialCenter;
+
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentLocation();
+  }
+
+  _getCurrentLocation() async {
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    setState(() {
+      initialCenter = LatLng(position.latitude, position.longitude);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBar(),
-      body: FlutterMap(
-        options: const MapOptions(
-          initialCenter: LatLng(40.3430942, -74.6550739),
-          maxZoom: 19,
-          minZoom: 15,
-          initialZoom: 15,
-        ),
-        children: [
-          TileLayer(
-            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-            maxZoom: 19,
-          ),
-          CurrentLocationLayer(),
-        ],
-      ),
+      body: initialCenter == null
+          ? Center(child: CircularProgressIndicator())
+          : FlutterMap(
+              options: MapOptions(
+                initialCenter: initialCenter!,
+                maxZoom: 19,
+                minZoom: 15,
+                initialZoom: 18,
+              ),
+              children: [
+                TileLayer(
+                  urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                  maxZoom: 19,
+                ),
+                CurrentLocationLayer(),
+              ],
+            ),
     );
   }
 }
