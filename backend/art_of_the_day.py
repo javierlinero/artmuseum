14b9b5453fb3296ddd10bc4f7cdf154669337841
Jeffrey Cheng <jeffreycheng1230@gmail.com>
2023-10-30 15:53:58 -0400

    added db + tinder endpoint
    use aws db
    
    use aws db for all db procedures

--- backend/art_of_the_day.py
+++ backend/art_of_the_day.py
@@ -1,7 +1,9 @@
 import json
 import random
 import os
+import flask
 from flask import Flask, jsonify
+import database as db
 
 app = Flask(__name__)
 
@@ -24,7 +26,7 @@ def art_of_the_day():
     data = get_json(file)
 
     title = data["displaytitle"]
-    artist = data["makers"]["displayname"]
+    #artist = data["makers"]["displayname"]
     imageUrl = data["primaryimage"]
     year = data["daterange"]
     materials = data["medium"]
@@ -33,7 +35,7 @@ def art_of_the_day():
 
     json_data = {
         "title": title,
-        "artist": artist,
+        #"artist": artist,
         "imageUrl": imageUrl,
         "year": year,
         "materials": materials,
@@ -42,3 +44,40 @@ def art_of_the_day():
     }
 
     return jsonify(json_data)
+
+@app.route('/tinder_for_art', methods=['GET'])
+def tinder_for_art_get():
+    userid = flask.request.args.get('userid')
+    pref = db.get_user_pref(userid)
+    print("Suggesting images based on preferences of userid " + str(userid) + " with preferences:")
+    print(pref)
+
+    file = get_random_file()
+    data = get_json(file)
+    # still images without url *************************
+    url = None
+    if len(data["primaryimage"]) < 1:
+        url = "https://miro.medium.com/v2/resize:fit:800/1*hFwwQAW45673VGKrMPE2qQ.png"
+    else:
+        url = data["primaryimage"][0]
+
+    json_data = {
+        "url": url
+    }
+
+    return jsonify(json_data)
+
+@app.route('/tinder_for_art', methods=['POST'])
+def tinder_for_art_post():
+    data = flask.request.form
+    userid = int(data["userid"])
+    artobj = data["artobj"]
+    rating = int(data["rating"])
+
+    print("Updating pref of userid" + str(userid) + " with (" + artobj + "," + str(rating) + ")")
+
+    db.set_user_pref(userid, (artobj, int(rating)))
+
+    return "SUCCESS\n"
+
+#curl "http://172.18.65.52:10203/tinder_for_art?userid=1" -F userid=1 -F artobj='artobject_4863.json' -F rating=-1
