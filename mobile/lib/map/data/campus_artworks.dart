96b248dfb9b7a737dab0808ea5236999615577ca
Nathan Beck <natebeck18@gmail.com>
2023-11-05 01:22:50 -0400

    East pyne stuff

--- mobile/lib/map/data/campus_artworks.dart
+++ mobile/lib/map/data/campus_artworks.dart
@@ -56,6 +56,87 @@ List<CampusArtwork> campusArtworks = [
     lat: 40.3486,
     long: -74.6566,
   ),
+  CampusArtwork(
+    name: 'Two Planes Vertical Horizontal II',
+    year: '1970',
+    artist: 'George Rickey',
+    imageUrl:
+        'https://puam-loris.aws.princeton.edu/loris/y1972-42.jp2/full/pct:25/0/default.jpg',
+    lat: 40.348378,
+    long: -74.657904,
+  ),
+  CampusArtwork(
+    name: 'Public Table',
+    year: '1978-79',
+    artist: 'Scott Burton',
+    imageUrl:
+        'https://puam-loris.aws.princeton.edu/loris/y1980-11_0006.jp2/full/pct:25/0/default.jpg',
+    lat: 40.348354,
+    long: -74.658345,
+  ),
+  CampusArtwork(
+    name: 'John Witherspoon (1723-1794)',
+    year: '2001',
+    artist: 'Alexander Stoddart',
+    imageUrl:
+        'https://puam-loris.aws.princeton.edu/loris/PP635_a.jp2/full/pct:25/0/default.jpg',
+    lat: 40.3486,
+    long: -74.658,
+  ),
+  CampusArtwork(
+    name: 'John Witherspoon (1723-1794)',
+    year: '1897',
+    artist: 'John Massey Rhind',
+    imageUrl:
+        'https://puam-loris.aws.princeton.edu/loris/pp22_2017.jp2/full/pct:25/0/default.jpg',
+    lat: 40.348682,
+    long: -74.658511,
+  ),
+  CampusArtwork(
+    name: 'James Madison, Class of 1771 (1751-1836)',
+    year: '1897',
+    artist: 'John Massey Rhind',
+    imageUrl:
+        'https://puam-loris.aws.princeton.edu/loris/pp173_2017.jp2/full/pct:25/0/default.jpg',
+    lat: 40.348698,
+    long: -74.658646,
+  ),
+  CampusArtwork(
+    name: 'Oliver Ellsworth (1745-1807)',
+    year: '1897',
+    artist: 'John Massey Rhind',
+    imageUrl:
+        'https://puam-loris.aws.princeton.edu/loris/pp166_2017.jp2/full/pct:25/0/default.jpg',
+    lat: 40.348823,
+    long: -74.658688,
+  ),
+  CampusArtwork(
+    name: 'James McCosh (1811-1894)',
+    year: '1897',
+    artist: 'John Massey Rhind',
+    imageUrl:
+        'https://puam-loris.aws.princeton.edu/loris/pp38_2017.jp2/full/pct:25/0/default.jpg',
+    lat: 40.348776,
+    long: -74.658548,
+  ),
+  CampusArtwork(
+    name: 'Clouds Nine',
+    year: '2002',
+    artist: 'Win Knowlton',
+    imageUrl:
+        'https://puam-loris.aws.princeton.edu/loris/STU00209.jp2/full/pct:25/0/default.jpg',
+    lat: 40.3488,
+    long: -74.6585,
+  ),
+  CampusArtwork(
+    name: 'Remembrance',
+    year: '2000',
+    artist: 'Toshiko Takaezu',
+    imageUrl:
+        'https://puam-loris.aws.princeton.edu/loris/PP638_DET1.jp2/full/pct:25/0/default.jpg',
+    lat: 40.3489,
+    long: -74.6587,
+  ),
 
   // Graduate college
   CampusArtwork(
