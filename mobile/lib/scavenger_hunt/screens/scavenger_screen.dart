604fc4bdaf297cddf25d600975d537c7c16be6d0
Nathan Beck <natebeck18@gmail.com>
2023-11-29 22:23:52 -0500

    Implement the images with blurs

--- mobile/lib/scavenger_hunt/screens/scavenger_screen.dart
+++ mobile/lib/scavenger_hunt/screens/scavenger_screen.dart
@@ -54,7 +54,7 @@ class _ScavengerHuntScreenState extends State<ScavengerHuntScreen> {
 
   Widget _buildInitialScreen(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Start Scavenger Hunt')),
+      appBar: appBar(),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
@@ -101,7 +101,7 @@ class _ScavengerHuntScreenState extends State<ScavengerHuntScreen> {
   Widget _buildInProgressScreen(
       ScavengerHuntInProgress state, BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Scavenger Hunt In Progress')),
+      appBar: appBar(),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -132,17 +132,22 @@ class _ScavengerHuntScreenState extends State<ScavengerHuntScreen> {
       return CircularProgressIndicator();
     } else {
       return ClipRect(
-        // Clip the image to its bounds
-        child: ImageFiltered(
-          imageFilter: ImageFilter.blur(
-            sigmaX: calculateBlurLevel(state.distance),
-            sigmaY: calculateBlurLevel(state.distance),
+        child: ConstrainedBox(
+          constraints: BoxConstraints(
+            maxHeight: 500,
           ),
-          child: CachedNetworkImage(
-            imageUrl: '${state.currentTarget.imageUrl}/full/full/0/default.jpg',
-            fit: BoxFit.contain,
-            placeholder: (context, url) => CircularProgressIndicator(),
-            errorWidget: (context, url, error) => Icon(Icons.error),
+          child: ImageFiltered(
+            imageFilter: ImageFilter.blur(
+              sigmaX: calculateBlurLevel(state.distance),
+              sigmaY: calculateBlurLevel(state.distance),
+            ),
+            child: CachedNetworkImage(
+              imageUrl:
+                  '${state.currentTarget.imageUrl}/full/full/0/default.jpg',
+              fit: BoxFit.contain,
+              placeholder: (context, url) => CircularProgressIndicator(),
+              errorWidget: (context, url, error) => Icon(Icons.error),
+            ),
           ),
         ),
       );
@@ -151,7 +156,7 @@ class _ScavengerHuntScreenState extends State<ScavengerHuntScreen> {
 
   Widget _buildCompletedScreen() {
     return Scaffold(
-      appBar: AppBar(title: Text('Scavenger Hunt Completed')),
+      appBar: appBar(),
       body: Center(
           child:
               Text('Congratulations! You have completed the scavenger hunt.')),
@@ -161,7 +166,6 @@ class _ScavengerHuntScreenState extends State<ScavengerHuntScreen> {
   Widget _buildDefaultScreen(ScavengerHuntState state) {
     debugPrint(state.toString());
     if (state is ScavengerHuntError) {
-      debugPrint('meow');
       debugPrint(state.message);
     }
     return Scaffold(
