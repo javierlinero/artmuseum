27e173bcd566b945e93d065f840dfa47d160f46b
Nathan Beck <natebeck18@gmail.com>
2023-12-01 11:40:11 -0500

    Remove dio options

--- mobile/lib/search/domain/services/search_services.dart
+++ mobile/lib/search/domain/services/search_services.dart
@@ -2,20 +2,14 @@ import 'package:dio/dio.dart';
 import 'package:puam_app/search/index.dart';
 
 class SearchService {
-  Dio dio = Dio()
-    ..options = BaseOptions(
-      baseUrl: 'http://puamdns.ddns.net:8080',
-      connectTimeout: Duration(seconds: 50),
-      receiveTimeout: Duration(seconds: 30),
-    )
-    ..interceptors.add(LogInterceptor(responseBody: true)); // Enable Logging
+  Dio dio = Dio();
   final String baseurl = 'http://puamdns.ddns.net:8080';
 
   Future<List<SearchArtwork>> fetchArtworks(
       {String? query, int? year, int? limit, int? offset}) async {
     try {
       var response = await dio.get(
-        '/search',
+        '$baseurl/search',
         data: {
           'query': query,
           'year': year,
