7aeb668e0471d4e029a76910979478aa95a3dbf6
Nathan Beck <natebeck18@gmail.com>
2023-12-05 14:51:17 -0500

    Full screen photos (#46)
    * Allow full screening in:
    - AOTD
    - Favorites
    
    * add to tinder for art
    
    * comment
    
    * Allow lots of zoom
    
    * Dynamically show the fullscreen

--- mobile/lib/art_of_the_day/widgets/painting.dart
+++ mobile/lib/art_of_the_day/widgets/painting.dart
@@ -1,7 +1,10 @@
-// ignore_for_file: prefer_const_constructors_in_immutables
+// ignore_for_file: prefer_const_constructors_in_immutables, prefer_const_constructors
 
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:photo_view/photo_view.dart';
+
+import '../../shared/widgets/full_screen_photo_view.dart';
 
 class PaintingWidget extends StatelessWidget {
   final String imageUrl;
@@ -15,14 +18,36 @@ class PaintingWidget extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 10.0),
       child: Center(
-        child: Container(
+        child: Stack(
           alignment: Alignment.center,
-          child: CachedNetworkImage(
-            imageUrl: '$imageUrl/full/full/0/default.jpg',
-            fit: BoxFit.contain,
-            placeholder: (context, url) => CircularProgressIndicator(),
-            errorWidget: (context, url, error) => Icon(Icons.error),
-          ),
+          children: [
+            CachedNetworkImage(
+              placeholder: (context, url) => CircularProgressIndicator(),
+              errorWidget: (context, url, error) => Icon(Icons.error),
+              fit: BoxFit.contain,
+              imageUrl: '$imageUrl/full/pct:25/0/default.jpg',
+              imageBuilder: (context, imageProvider) {
+                return PhotoView(
+                  imageProvider: imageProvider,
+                );
+              },
+            ),
+            Positioned(
+              bottom: 10,
+              right: 10,
+              child: IconButton(
+                icon: Icon(Icons.zoom_out_map, color: Colors.white),
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) =>
+                          FullScreenPhotoView(imageUrl: imageUrl),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
