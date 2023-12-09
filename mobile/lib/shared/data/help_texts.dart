3a001d71d4af81666826f3ae6de061d4974cb831
Nathan Beck <natebeck18@gmail.com>
2023-12-08 20:28:23 -0500

    Map and tinder help texts

--- mobile/lib/shared/data/help_texts.dart
+++ mobile/lib/shared/data/help_texts.dart
@@ -1,4 +1,15 @@
 class HelpData {
   static String aotdHelp =
       'The Art of the Day page displays a customized art of the day for those logged in, and a random one for all logged out users.';
+  static String tinderHelp = '- Swipe left to dislike art\n'
+      '- Swipe right to like art\n'
+      '- Swipe up to rate it neutrally';
+  static String mapHelp = '- Click orange icons to view art\n'
+      '- Click top left button to start scavenger hunt\n'
+      '- Click location button in bottom left to focus your location';
+  static String intialScavengerHelp =
+      '- Change slider to choose amount of artwork\n'
+      '- Click start to begin';
+  static String progressScavengerHelp = '- Walk to the artwork\n'
+      '- Click "Found" to advance to next art';
 }
