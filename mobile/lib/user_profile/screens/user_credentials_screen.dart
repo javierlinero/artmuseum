387b70354cfa7a5a75426b8551138402f6ad0b25
Nathan Beck <natebeck18@gmail.com>
2023-12-11 03:41:19 -0500

    Form validation implementation

--- mobile/lib/user_profile/screens/user_credentials_screen.dart
+++ mobile/lib/user_profile/screens/user_credentials_screen.dart
@@ -20,6 +20,10 @@ class _UserCredentialsState extends State<UserCredentials> {
       TextEditingController();
 
   bool _isSignUpButtonEnabled = false;
+  bool _emailValid = true;
+  bool _isPasswordMatching = true;
+  bool _isPasswordLongEnough = true;
+  bool _isDisplayNameNotEmpty = true;
 
   void _onSubmit(BuildContext context) {
     BlocProvider.of<AuthBloc>(context).add(
@@ -38,6 +42,25 @@ class _UserCredentialsState extends State<UserCredentials> {
     return regex.hasMatch(email);
   }
 
+  void _validateForm() {
+    bool isEmailValid = _isEmailValid(_controllerEmail.text);
+    bool isPasswordMatching =
+        _controllerPassword.text == _controllerConfirmPassword.text;
+    bool isPasswordLongEnough = _controllerPassword.text.length >= 6;
+    bool isDisplayNameNotEmpty = _controllerDisplayName.text.isNotEmpty;
+
+    setState(() {
+      _emailValid = isEmailValid;
+      _isPasswordMatching = isPasswordMatching;
+      _isPasswordLongEnough = isPasswordLongEnough;
+      _isDisplayNameNotEmpty = isDisplayNameNotEmpty;
+      _isSignUpButtonEnabled = isPasswordMatching &&
+          isPasswordLongEnough &&
+          isDisplayNameNotEmpty &&
+          isEmailValid;
+    });
+  }
+
   @override
   void initState() {
     super.initState();
@@ -45,16 +68,6 @@ class _UserCredentialsState extends State<UserCredentials> {
     _controllerConfirmPassword.addListener(_validateForm);
     _controllerDisplayName.addListener(_validateForm);
     _controllerEmail.addListener(_validateForm);
-  }
-
-  void _validateForm() {
-    setState(() {
-      _isSignUpButtonEnabled =
-          _controllerPassword.text == _controllerConfirmPassword.text &&
-              _controllerPassword.text.isNotEmpty &&
-              _controllerDisplayName.text.isNotEmpty &&
-              _isEmailValid(_controllerEmail.text);
-    });
   }
 
   @override
@@ -80,6 +93,7 @@ class _UserCredentialsState extends State<UserCredentials> {
   }
 
   Widget _buildUserCredentialsPage(BuildContext context) {
+    const double errorTextHeight = 15;
     return SingleChildScrollView(
       child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
         Text(
@@ -128,6 +142,15 @@ class _UserCredentialsState extends State<UserCredentials> {
             ),
           ),
         ),
+        SizedBox(
+          height: errorTextHeight,
+          child: _emailValid || _controllerEmail.text.isEmpty
+              ? null
+              : Text(
+                  'Invalid email address',
+                  style: TextStyle(color: Colors.red, fontSize: 12),
+                ),
+        ),
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
           child: TextFormField(
@@ -151,6 +174,15 @@ class _UserCredentialsState extends State<UserCredentials> {
               ),
             ),
           ),
+        ),
+        SizedBox(
+          height: errorTextHeight,
+          child: _isDisplayNameNotEmpty
+              ? null
+              : Text(
+                  'Display name cannot be empty',
+                  style: TextStyle(color: Colors.red, fontSize: 12),
+                ),
         ),
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
@@ -177,6 +209,15 @@ class _UserCredentialsState extends State<UserCredentials> {
             ),
           ),
         ),
+        SizedBox(
+          height: errorTextHeight,
+          child: _isPasswordLongEnough
+              ? null
+              : Text(
+                  'Password must be at least 6 characters long',
+                  style: TextStyle(color: Colors.red, fontSize: 12),
+                ),
+        ),
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
           child: TextFormField(
@@ -201,6 +242,15 @@ class _UserCredentialsState extends State<UserCredentials> {
               ),
             ),
           ),
+        ),
+        SizedBox(
+          height: errorTextHeight,
+          child: _isPasswordMatching || _controllerPassword.text.isEmpty
+              ? null
+              : Text(
+                  'Passwords do not match',
+                  style: TextStyle(color: Colors.red, fontSize: 12),
+                ),
         ),
         ElevatedButton(
           onPressed: _isSignUpButtonEnabled
